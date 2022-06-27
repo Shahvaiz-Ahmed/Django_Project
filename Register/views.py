95c89742ded8d65593840c7d3fc4c7df2ea13b73
unknown <shahvaiz123satti@gmail.com>
2022-06-27 10:17:58 -0700

    first commit

---
+++ Register/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect("/loginuser")
+        else:
+            messages.success(request, 'Must fill all fields correctly!')
+            form = CustomUserCreationForm()
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register/register.html', {'form':form})
